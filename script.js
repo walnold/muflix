b2a9b02c25a54c96f41077639609757c9fda0065
Walter <2760walter@gmail.com>
2025-08-02 10:56:29 +0300

    add image placeholder for null poster_path

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const API_KEY = "921c8c5f410e6e8d583d40a923d3847d";
 const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
 const IMG_PATH = "https://images.tmdb.org/t/p/w1280";
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`;
+const IMG_PLACEHOLDER = 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
@@ -14,13 +15,17 @@ function showMovies(movies) {
     main.innerHTML = ''
     movies.forEach((movie) => {
         const { title, poster_path, vote_average, overview } = movie
+        console.log(poster_path)
+        console.log(overview)
         
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-        <img src="${IMG_PATH+poster_path}"
+        <img src="${
+            poster_path == null?IMG_PLACEHOLDER:
+            IMG_PATH + poster_path}"
                 alt="">
             <div class="movie-info">
                 <h3>${title}</h3>
@@ -70,8 +75,9 @@ form.addEventListener("submit", (e) => {
 getMovies(API_URL);
 
 async function getMovies(url) {
-  console.log("getting movies");
+  
   const res = await fetch(url);
-  const data = await res.json();
+    const data = await res.json();
+    console.log(data.results)
   showMovies(data.results)
 }
